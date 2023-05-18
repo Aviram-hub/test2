4e4b7dbd53268d19d63fda5dda3275327901526e
Kunqing Deng <dengkunqing@scutech.com>
2023-05-18 11:24:40 +0800

    2xiugai aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -6,4 +6,6 @@ int main(int argc)
 {
 }
 // vim: set ts=4 sw=4 sts=4 et:
+xixi
 hshs
+右边终端的修改
--- bb.cpp
+++ bb.cpp
@@ -2,4 +2,7 @@
 int main
 
 xiugai bb
+2xiugai bb.cpp
+3xiugai bb.cpp
+左边终端的修改
 // vim: set ts=4 sw=4 sts=4 et:
